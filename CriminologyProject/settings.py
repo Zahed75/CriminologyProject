3bf0d43291a03c06b2885fe89c27d20324f14bda
Zahed75 <Zahedhasan.CS@gmail.com>
2021-07-24 12:40:19 +0600

    server done

--- CriminologyProject/settings.py
+++ CriminologyProject/settings.py
@@ -93,16 +93,16 @@ DATABASES = {
 }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Criminology_db',
-        'USER': 'postgres',
-        'PASSWORD': '4455',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Criminology_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '4455',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
