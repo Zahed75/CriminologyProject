03b39195573a1ccbfd962a517c5a85caea644c73
Zahed75 <Zahedhasan.CS@gmail.com>
2021-07-24 12:12:16 +0600

    db done

--- CriminologyProject/settings.py
+++ CriminologyProject/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'CriminologyProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Criminology_db',
-        'USER': 'postgres',
-        'PASSWORD': '4455',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Criminology_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '4455',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
