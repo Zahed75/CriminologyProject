6d000424f899fedca63a224869dc9cb2861579ac
unknown <Zahedhasan.CS@gmail.com>
2021-08-14 02:36:53 +0600

    82th update

--- CriminologyProject/settings.py
+++ CriminologyProject/settings.py
@@ -94,25 +94,25 @@ WSGI_APPLICATION = 'CriminologyProject.wsgi.application'
 # }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'criminol_database',
-#         'USER': 'criminol_criminology',
-#         'PASSWORD': 'zxddutcriaa5',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-#
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'criminol_database',
+        'USER': 'criminol_criminology',
+        'PASSWORD': 'zxddutcriaa5',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
