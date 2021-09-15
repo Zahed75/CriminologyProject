8f5dcbd9c328c6fdd80a36c632e815e7dcc662c0
unknown <Zahedhasan.CS@gmail.com>
2021-09-15 15:05:24 +0600

    chnagesd the data

--- CriminologyProject/settings.py
+++ CriminologyProject/settings.py
@@ -2,7 +2,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django_adminlte3.settings import MIDDLEWARE
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
@@ -82,28 +81,28 @@ WSGI_APPLICATION = 'CriminologyProject.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Criminology_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '4455',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'criminol_database',
-        'USER': 'criminol_criminology',
-        'PASSWORD': 'zxddutcriaa5',
+        'NAME': 'Criminology_db',
+        'USER': 'postgres',
+        'PASSWORD': '4455',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'criminol_database',
+#         'USER': 'criminol_criminology',
+#         'PASSWORD': 'zxddutcriaa5',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
